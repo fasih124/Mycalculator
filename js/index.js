4417acd8a5a2d3dfa407e19df2c53c6666472473
fasih ahmed khan <fasihkhan124124@gamil.com>
2022-09-30 10:48:44 +0500

    Create operate function

--- js/index.js
+++ js/index.js
@@ -11,3 +11,28 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return subtract(a, b);
+      break;
+    case "*":
+      return multiply(a, b);
+      break;
+    case "/":
+      if (b === 0) {
+        return alert("Cannot Divide By Zero");
+      } else {
+        return Math.toFixed(divide(a, b));
+      }
+
+      break;
+    default:
+      console.log("error");
+      break;
+  }
+}
