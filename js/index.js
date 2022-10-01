051405d373367c142288e7130a7f1d350eac2af5
fasih ahmed khan <fasihkhan124124@gamil.com>
2022-10-01 13:25:35 +0500

    Done js for web

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+// variables
+let data = [];
+let operators = [];
+let value = [];
+let maxlength = 16;
+let ans = 0;
+
+// DOM variables
+let screen = document.querySelector(".result");
+const buttons = document.querySelectorAll(".button");
+const remove = document.querySelector(".button-remove");
+const clearScreen = document.querySelector(".button-clear");
+
+// functions
+
+function resetvalue() {
+  value = [];
+  data = [];
+  operators = [];
+  screen.textContent = value;
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -26,13 +48,84 @@ function operate(operator, a, b) {
     case "/":
       if (b === 0) {
         return alert("Cannot Divide By Zero");
+        break;
+      } else if (b < a) {
+        return divide(a, b).toFixed(2);
+        break;
       } else {
-        return Math.toFixed(divide(a, b));
+        return divide(a, b).toFixed(16);
+        break;
       }
-
-      break;
     default:
-      console.log("error");
+      return alert("error");
       break;
   }
 }
+
+// Populate data
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "+") {
+      data.push(Number(value.join("")));
+      value = [];
+      operators.push("+");
+    } else if (button.textContent === "=") {
+      //
+      console.log("you press equal");
+      data.push(Number(value.join("")));
+      let datalength = data.length - 1;
+      if (
+        operators.length !== datalength ||
+        operators.length == 0 ||
+        datalength === 0
+      ) {
+        alert("Syntax Error");
+        resetvalue();
+      } else if (operators.length === datalength) {
+        for (let i = 0; data.length !== 1; i++) {
+          ans = operate(operators[0], data[0], data[1]);
+          operators.shift();
+          data.shift();
+          data.shift();
+          data.unshift(ans);
+        }
+      }
+      //
+    } else if (button.textContent === "/") {
+      data.push(Number(value.join("")));
+      value = [];
+      operators.push("/");
+    } else if (button.textContent === "-") {
+      data.push(Number(value.join("")));
+      value = [];
+      operators.push("-");
+    } else if (button.textContent === "*") {
+      data.push(Number(value.join("")));
+      value = [];
+      operators.push("*");
+    } else {
+      value.push(`${button.textContent}`);
+    }
+    // making value not to overflow  data remian till maxlenght=16;
+    if (value.length <= maxlength) {
+      screen.textContent = value.join("");
+    } else {
+      screen.textContent = value.slice(value.length - maxlength).join("");
+    }
+    // display ans
+    if (button.textContent === "=") {
+      value = [ans];
+      screen.textContent = data.join("");
+      data = [];
+    }
+  });
+});
+
+// clear data
+clearScreen.addEventListener("click", resetvalue);
+
+//remove item
+remove.addEventListener("click", () => {
+  value.pop();
+  screen.textContent = value;
+});
